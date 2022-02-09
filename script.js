9a7679ca27c829a79aa6b695ec6891c2e190aab6
albertofanini-prog <albertofanini@outlook.com>
2022-02-09 13:53:48 +0100

    2nd Commit - Spiegazione

---
+++ script.js
@@ -0,0 +1,10 @@
+//Creare una lista esclusiva di user i quali possono accedere al servzio
+//Chiedere l'email all'user
+//Controllare che l'email riferita all'user sia nella lista esclusiva
+//Stampare un messaggio per l'user con l'esito del controllo
+
+
+//Generare due numeri random da 1 a 6
+//Assegnare un numero al cpu e un numero all'user
+//Stabilire il vincitore
+//E se i numeri sono pari?
