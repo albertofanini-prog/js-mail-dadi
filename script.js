0a297da7ba2d6b25a8e8ecf0564ea5593e73bda8
albertofanini-prog <albertofanini@outlook.com>
2022-02-09 14:47:55 +0100

    4th Commit - Verifica e-mail funzionante

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*-----Verifica user-----*/
+
 //Creare una lista esclusiva di user i quali possono accedere al servzio
 let emails = [
     'alberto@gmail.com',
@@ -8,11 +10,33 @@ let emails = [
 console.log(emails);
 
 //Chiedere l'email all'user
+let user = prompt('Benvenuto. Inserisci la tua mail per procedere.');
+console.log(user);
+
+//Dare un valore booleano alla variabile
+let search = false;
+console.log(search);
 
 //Controllare che l'email riferita all'user sia nella lista esclusiva
+for (let i = 0; i<emails.length; i++){
+    if(user === emails[i]){
+        search = true;
+    }
+}
+
 //Stampare un messaggio per l'user con l'esito del controllo
+if(search === true){
+    alert('Dato corretto. Clicca su ok per proseguire.');
+}   else{
+        alert('Dato non corretto. Ricaricare la pagina.');
+}
+
+//Prendere elemento html in cui scrivere
+//let firstHTML = document.querySelector('.users');
+//firstHTML.innerHTML = `L'e-mail da lei inserita è:`;
 
 
+/*-----Gioco dei dadi-----*/
 //Generare due numeri random da 1 a 6
 //Assegnare un numero al cpu e un numero all'user
 //Stabilire il vincitore
